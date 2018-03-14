120771affbe95995d6159d33d61c2a9fb7e16f90
Augustus Urschel <augustusurschel@gmail.com>
2018-03-14 16:01:18 -0400

    Add output to Puller. Add Ranker folder. Add ranker skeleton.

---
+++ Ranker/Ranker 1.0.py
@@ -0,0 +1,27 @@
+'''
+RANKER VERSION 1.0 (3.5.18)
+Py 3.X
+Augustus Urschel
+
+Objective:
+1) Import the Main Twitter data
+
+2) By [X]All-Time []Daily []Hourly:
+    3) Score Tweets on [X]Retweets []Likes []Patronage
+    4) Filter out bad tweets on [X] 0 Score []Profanity []Suspected AI []Other
+    5) Rank Tweets & Saveout Full data
+    6) Condense Data for Scoreboard & Saveout
+'''
+#Setup
+
+#1: Import the Main Twitter Data
+
+#2: Loop on All-Time
+
+#3: Score on Retweets
+
+#4: Filter out 0 Scores
+
+#5: Rank Tweets & Saveout
+
+#6: Condense to Scoreboard & Saveout
