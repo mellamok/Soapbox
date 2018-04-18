61bd0757ae4926b3ef130e8cc29ec285c8d978a8
Augustus Urschel <augustusurschel@gmail.com>
2018-04-18 19:42:48 -0400

    Updated Ranker to fit with the new cleaup code.

--- Ranker.py
+++ Ranker.py
@@ -25,7 +25,7 @@ import csv
 def find_file_location(filename):
     """Finds the main twitter data pull."""
     cwd = os.getcwd()
-    pullerdir = cwd + "\\Puller\\Output Data\\"
+    pullerdir = cwd + "\\Puller Data\\"
     inputloc = pullerdir + filename
     print("Data is : " + inputloc)
     return inputloc
@@ -44,9 +44,10 @@ def create_master_connection(dbname):
 def datapull_master(conn_master):
     """Pulls data from the master"""
     c = conn_master.cursor()
-    #TODO: Change the data types in the puller to not be all string.
-    c.execute("SELECT tweet_id, created_at, from_user_screen_name, from_user_id, retweet_count, content \
-    FROM hashtags WHERE entities_media_count = '' AND retweeted_status = '' AND truncated = 0 ORDER BY retweet_count ASC")
+    #TODO: Filter out those without soapbox hashtags
+    #Also find a way to combine retweets + favs
+    c.execute("SELECT tweet_id, created_at, from_user_screen_name, from_user_id, favorite_count, retweet_count, content \
+    FROM hashtags WHERE language = 'en' AND entities_media_count = 0 AND retweeted_status = '' AND truncated = 0 ORDER BY favorite_count DESC")
     data = c.fetchall() #note: This reads all data in the cursor to memory. Will cause performance issues. Change to "for row in c: print row"
     return data
 
@@ -55,10 +56,10 @@ def datapull_master(conn_master):
 
 def to_csv(dataframe):
     cwd = os.getcwd()
-    savedir = cwd + "\\Ranker\\Output\\"
+    savedir = cwd + "\\CSV Output\\"
     csvpath = savedir + "output.csv"
     with open(csvpath, 'w', newline='', encoding='utf-8') as csvfile: #utf-16 also works(ish)
-        fieldnames = ['Tweet_ID', 'Tweet_Date', 'User_Name', 'User_ID', 'Retweet_CT', 'Content']
+        fieldnames = ['Tweet_ID', 'Tweet_Date', 'User_Name', 'User_ID', 'Favorite_CT', 'Retweet_CT', 'Content']
         dictwriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
         datawriter = csv.writer(csvfile, 'excel')
         dictwriter.writeheader()
@@ -69,26 +70,11 @@ def to_csv(dataframe):
     #for row in dataframe:
     #    outputcsv.writerow(row)
 
-
-
-#vars to pull:
-#tweet_id, truncated, retweeted_status, created_at, content, from_user_screen_name, from_user_id, retweet_count
-
-#need new vars: str ver of user_created_at
 #filters: date of user created
-
-
-#3: Prog to Score on Retweets
-
-#4: Prog to Filter out 0 Scores
-
-#5: Prog to Rank Tweets & Saveout
-
-#6: Prog to Condense to Scoreboard & Saveout
 
 #Run
 if __name__ == "__main__":
-    data_master_loc = find_file_location("Data_2.28.2018.sqlite") #define the main data
+    data_master_loc = find_file_location("Testfile 2018 4 18 19 10.sqlite") #define the main data
     conn_master = create_master_connection(data_master_loc) #connect to it
     x = datapull_master(conn_master) #Pull all data from hastags table and display it
     for row in x:
