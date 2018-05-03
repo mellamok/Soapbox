99562175b38822cfdb78ab3436650a84b432481a
Augustus Urschel <augustusurschel@gmail.com>
2018-05-03 18:20:14 -0400

    Classified Ranker, updated Master to fit.

--- Master Scheduler.py
+++ Master Scheduler.py
@@ -8,23 +8,28 @@ def cycle_1_min():
     now = datetime.datetime.now()
 
     #File components
+    iteration = "%d %d %d %d %d" % (now.year, now.month, now.day, now.hour, now.minute)
     iteration_file_puller = "Puller %d %d %d %d %d.sqlite"  % (now.year, now.month, now.day, now.hour, now.minute)
     iteration_file_ranker = "Ranker %d %d %d %d %d.csv" % (now.year, now.month, now.day, now.hour, now.minute)
 
+    #Do Puller
     print("I play at every half minute in the day. The time is: ", datetime.datetime.now())
     print("Puller file = ", iteration_file_puller)
     startpull = Puller.Scrape(iteration_file_puller)
     startpull.main()
-
+    
+    #Do Alltime Ranker
     print("Ranker file = ", iteration_file_ranker)
-    #Make a class and this not look like poop later.
-    data_master_loc = Ranker.find_file_location(iteration_file_puller) #define the main data
+    alltime = Ranker.Ranker(iteration, "alltime")
+    alltime.main()
+    
+    """ data_master_loc = Ranker.find_file_location(iteration_file_puller) #define the main data
     conn_master = Ranker.create_master_connection(data_master_loc) #connect to it
     x = Ranker.datapull_master(conn_master) #Pull all data from hastags table and display it
     for row in x:
         print(row)
     Ranker.to_csv(x, iteration_file_ranker)
-    #
+    # """
 scheduler=BlockingScheduler()
 scheduler.add_job(cycle_1_min, 'cron', second=1)
 scheduler.start()
--- Ranker.py
+++ Ranker.py
@@ -3,13 +3,10 @@ RANKER VERSION 1.0 (3.5.18)
 Py 3.X
 Augustus Urschel
 
-Objective:
-1) Import the Main Twitter data
-
 By [X]All-Time []Daily []Hourly:
     2) Pull relevant data from Master
     3) Filter out bad tweets on [X] 0 Score []Profanity []Suspected AI []Other
-    4) Score & Rank Tweets on [X]Retweets []Likes []Patronage
+    4) Score & Rank Tweets on [X]Retweets [X]Likes []Patronage
     5) Save for Scoreboard
 '''
 #Setup
@@ -20,63 +17,62 @@ from sqlite3 import Error
 import sqlalchemy
 import csv
 
-#1: Prog to Import the Main Twitter Data
+class Ranker:
+    def __init__(self, iteration, cycle):
+        self.cycle = cycle
+        self.iteration = iteration
+        self.iteration_input = "Puller {iteration}.sqlite".format(iteration=iteration)
+        self.iteration_output = "Ranker {iteration} {cycle}.csv".format(iteration=iteration, cycle=cycle)
 
-def find_file_location(filename):
-    """Finds the main twitter data pull."""
-    cwd = os.getcwd()
-    pullerdir = cwd + "\\Puller Data\\"
-    inputloc = pullerdir + filename
-    print("Data is : " + inputloc)
-    return inputloc
+    def create_master_connection(self):
+        """Connects to the maintwitter data pull"""
+        self.cwd = os.getcwd()
+        self.pullerdir = self.cwd + "\\Puller Data\\"
+        self.inputloc = self.pullerdir + self.iteration_input
+        try:
+            self.conn_master = sqlite3.connect(self.inputloc)
+            return self.conn_master
+        except Error as e:
+            print(e)
 
-def create_master_connection(dbname):
-    """Connects to the maintwitter data pull"""
-    try:
-        conn_master = sqlite3.connect(dbname)
-        return conn_master
-    except Error as e:
-        print(e)
+        return None
 
-    return None
+    def datapull_master(self, connect):
+        """Pulls data from the master"""
+        self.c = connect.cursor()
+        self.c.execute("SELECT tweet_id, created_at, from_user_screen_name, from_user_id, favorite_count, retweet_count, content \
+        FROM hashtags WHERE language = 'en' AND entities_media_count = 0 AND retweeted_status = '' AND truncated = 0 ORDER BY favorite_count DESC")
+        self.data = self.c.fetchall() #note: This reads all data in the cursor to memory. Will cause performance issues. Change to "for row in c: print row"
+        return self.data
 
-# 3 Pull relevant data from Master
-def datapull_master(conn_master):
-    """Pulls data from the master"""
-    c = conn_master.cursor()
-    #TODO: Filter out those without soapbox hashtags
-    #Also find a way to combine retweets + favs
-    c.execute("SELECT tweet_id, created_at, from_user_screen_name, from_user_id, favorite_count, retweet_count, content \
-    FROM hashtags WHERE language = 'en' AND entities_media_count = 0 AND retweeted_status = '' AND truncated = 0 ORDER BY favorite_count DESC")
-    data = c.fetchall() #note: This reads all data in the cursor to memory. Will cause performance issues. Change to "for row in c: print row"
-    return data
+        #for row in rows:
+        #   print(row)
 
-    #for row in rows:
-    #   print(row)
+    def to_csv(self, dataframe, iteration_output):
+        """Write the data to a csv file"""
+        self.cwd = os.getcwd()
+        self.savedir = self.cwd + "\\CSV Output\\"
+        self.csvpath = self.savedir + iteration_output
+        with open(self.csvpath, 'w', newline='', encoding='utf-8') as self.csvfile: #utf-16 also works(ish)
+            self.fieldnames = ['Tweet_ID', 'Tweet_Date', 'User_Name', 'User_ID', 'Favorite_CT', 'Retweet_CT', 'Content']
+            self.dictwriter = csv.DictWriter(self.csvfile, fieldnames=self.fieldnames)
+            self.datawriter = csv.writer(self.csvfile, 'excel')
+            self.dictwriter.writeheader()
+            for row in dataframe:
+                self.datawriter.writerow(row)
 
-def to_csv(dataframe, filename):
-    cwd = os.getcwd()
-    savedir = cwd + "\\CSV Output\\"
-    csvpath = savedir + filename
-    with open(csvpath, 'w', newline='', encoding='utf-8') as csvfile: #utf-16 also works(ish)
-        fieldnames = ['Tweet_ID', 'Tweet_Date', 'User_Name', 'User_ID', 'Favorite_CT', 'Retweet_CT', 'Content']
-        dictwriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        datawriter = csv.writer(csvfile, 'excel')
-        dictwriter.writeheader()
-        for row in dataframe:
-            datawriter.writerow(row)
+        #outputcsv = csv.writer(open(csvpath, "w"))
+        #for row in dataframe:
+        #    outputcsv.writerow(row)
 
-    #outputcsv = csv.writer(open(csvpath, "w"))
-    #for row in dataframe:
-    #    outputcsv.writerow(row)
-
-#filters: date of user created
+    def main(self):
+        self.connect = self.create_master_connection()
+        self.x = self.datapull_master(self.connect)
+        for row in self.x:
+            print(row)
+        self.to_csv(self.x, self.iteration_output)
 
 #Run
 if __name__ == "__main__":
-    data_master_loc = find_file_location("Testfile 2018 4 18 19 10.sqlite") #define the main data
-    conn_master = create_master_connection(data_master_loc) #connect to it
-    x = datapull_master(conn_master) #Pull all data from hastags table and display it
-    for row in x:
-        print(row)
-    to_csv(x, "testfile")
+    x = Ranker("2018 4 19 20 54", "alltime")
+    x.main()
