ac17a4a86f24f1e0d6e59b8236a32d82b7db900b
Augustus Urschel <augustusurschel@gmail.com>
2018-07-17 20:06:23 -0400

    Trained popcorn to find next ID based on tweetid and tweetdate

--- Popcorn.py
+++ Popcorn.py
@@ -42,27 +42,71 @@ class Popcorn:
             print(e)
         return None
 
-    # def find_next_id(self):
-    # """Given the last tweetid and tweetdate, find the next id to print"""
-
     def print_tweet(self, connect, nextid):
         """Sent the tweet content to the visualizer"""
-        self.newtable = 'modified'
+        self.table = 'modified'
         self.vars ='from_user_screen_name, score, content'
         self.idcolumn = 'rowid'
         self.idnum = nextid
         self.c = connect.cursor()
-        self.c.execute("SELECT {vars} FROM {tn} WHERE {idcol} == {idnum}" \
-            .format(vars=self.vars, tn=self.newtable, idcol=self.idcolumn, idnum=self.idnum))
 
-        self.data = self.c.fetchall() 
-        return self.data
+        self.c.execute("SELECT {vars} FROM {tn} WHERE {idcol} == {idnum}" \
+            .format(vars=self.vars, tn=self.table, idcol=self.idcolumn, idnum=self.idnum))
+        self.displaydata = self.c.fetchone() 
+        return self.displaydata
+    
+    def find_last_tweet_id(self, connect, nextid):
+        """update the last lasttweetid """
+        self.table = 'modified'
+        self.idcolumn = 'rowid'
+        self.idnum = nextid
+        self.tweetidcolumn = 'tweet_id'
+        self.c = connect.cursor()
+
+        self.c.execute("SELECT {vars} FROM {tn} WHERE {idcol} == {idnum}" \
+            .format(vars=self.tweetidcolumn, tn=self.table, idcol=self.idcolumn, idnum=self.idnum))
+        self.lasttweetid = self.c.fetchone()
+        return self.lasttweetid
+
+    def find_last_tweet_date(self, connect, nextid):
+        """update the last lasttweetdate"""
+        self.newtable = 'modified'
+        self.idcolumn = 'rowid'
+        self.idnum = nextid
+        self.tweetcreatedatcolumn = 'created_at'
+        self.c = connect.cursor()
+
+        self.c.execute("SELECT {vars} FROM {tn} WHERE {idcol} == {idnum}" \
+            .format(vars=self.tweetcreatedatcolumn, tn=self.table, idcol=self.idcolumn, idnum=self.idnum))
+        self.lasttweetdate = self.c.fetchone()
+        return self.lasttweetdate
+    
+    def find_next_id(self, connect, lasttweetid, lasttweetdate):
+        """Given the last tweetid and tweetdate, find the next id to print"""
+        self.table = 'modified'
+        self.idcolumn = 'rowid'
+        self.tweetidcolumn = 'tweet_id'
+        self.tweetcreatedatcolumn = 'created_at'
+        self.lasttweetid = lasttweetid
+        self.lasttweetdate = lasttweetdate
+        self.c = connect.cursor()
+
+        #find id of old tweet
+        self.c.execute("SELECT {vars} FROM {tn} WHERE {tweetidcol} == {lasttweetidnum} AND {datecol} == '{lasttweetdate}'" \
+            .format(vars=self.idcolumn, tn=self.table, tweetidcol=self.tweetidcolumn, lasttweetidnum=self.lasttweetid, datecol=self.tweetcreatedatcolumn, lasttweetdate=self.lasttweetdate))    
+        #iterate to next id 
+        self.oldid = self.c.fetchone()
+        self.newid = self.oldid[0] + 1
+        return self.newid
 
     def main(self):
         self.connect = self.create_master_connection()
-        self.x = self.print_tweet(self.connect, self.nextid)
-        for row in self.x:
+        self.displaydata = self.print_tweet(self.connect, self.nextid)
+        for row in self.displaydata:
             print(row)
+        self.lasttweetdate = self.find_last_tweet_date(self.connect, self.nextid)
+        self.lasttweetid = self.find_last_tweet_id(self.connect, self.nextid)
+        self.nextid = self.find_next_id(self.connect, self.lasttweetid[0], self.lasttweetdate[0])
         self.connect.close()
 
 #Run as test
@@ -70,7 +114,6 @@ if __name__ == "__main__":
     fakedate = datetime.datetime(2018, 4, 19, hour=20, minute=52, second=7)
     fakedate_str = "%d %d %d %d %d" % (fakedate.year, fakedate.month, fakedate.day, fakedate.hour, fakedate.minute)
     x = Popcorn(fakedate, fakedate_str, "daily", None, None)
-    print(x.nextid)
-    print(x.lasttweetdate)
-    print(x.lasttweetid)
+    x.main()
+    x.main()
     x.main()
