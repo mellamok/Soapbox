44c84572fb5702ce6cb0c51f00f941043e054513
EI User <ei_auction_user@EI-Auction-MBP.local>
2018-09-16 21:00:56 -0400

    First pass at updating puller for django.

--- SoapBox/models.py
+++ SoapBox/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class User(models.Model):
+    """Represents a SoapBox user"""
 
     name = models.CharField(
 		max_length=15,
@@ -13,9 +14,36 @@ class User(models.Model):
 
 
 class Tweet(models.Model):
+    """Represents a tweet and its ranking"""
 
-    message = models.CharField(
+    tweet_id = models.BigIntegerField(
+        primary_key=True,
+        help_text='Twitter assigned id for tweet')
+    tweet_time = models.DateField()
+    scrape_time = models.DateField(
+        auto_now_add=True,
+        help_text='Time tweet was added')
+    last_updated_time = models.DateField(
+        auto_now=True,
+        help_text='Time of last update')
+    content = models.CharField(
         max_length=280,
         help_text='Tweet text content')
+    truncated = models.BooleanField(
+        default=False,
+        help_text='Is tweet truncated')
+    language = models.CharField(
+        max_length=2,
+        help_text='Language of tweet')
+    possibly_sensitive = models.BooleanField(
+        default=False,
+        help_text='Is tweet sensative')
+    retweeted_status = models.BooleanField(
+        default=False,
+        help_text='Is tweet a retweet')
+    retweet_count = models.PositiveIntegerField(
+        help_text='Number of retweets')
+    favorite_count = models.PositiveIntegerField(
+        help_text='Number of favorites')
     score = models.PositiveSmallIntegerField(
         help_text='SoapBox score given to tweet')
